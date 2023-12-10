636264740abb99502b19c99e527892b817eee7d7
Kirill Kosykh <triplesonicslash@list.ru>
2023-12-10 19:50:19 +0300

    FraudApplication is ok CustomerApplication is ok Starting eureka

--- customer/src/main/java/com/colossus/customer/CustomerService.java
+++ customer/src/main/java/com/colossus/customer/CustomerService.java
@@ -1,9 +1,10 @@
 package com.colossus.customer;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+public record CustomerService(CustomerRepository customerRepository, RestTemplate restTemplate) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -12,6 +13,14 @@ public record CustomerService(CustomerRepository customerRepository) {
                 .build();
         // todo: check if email valid
         // todo: check if email is not taken
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+        // todo: check if fraudster
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject("http://localhost:8091/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class, customer.getId()
+        );
+
+        if (fraudCheckResponse.isFraudster()) throw new IllegalStateException("fraudster");
+
+        // todo: send notification
     }
 }
---
+++ customer/src/main/java/com/colossus/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.colossus.customer;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
