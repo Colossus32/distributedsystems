98be786a16a76f06298f0baca123fe2950fcb968
Colossus <triplesonicslash@list.ru>
2023-12-17 23:22:07 +0300

    Set up RabbitMQ producer is ok

--- notification/src/main/java/com/colossus/notification/NotificationApplication.java
+++ notification/src/main/java/com/colossus/notification/NotificationApplication.java
@@ -1,13 +1,26 @@
 package com.colossus.notification;
 
+import com.colossus.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.colossus.notification", "com.colossus.amqp"})
 @EnableFeignClients(basePackages = "com.colossus.clients")
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+    /*@Bean
+    CommandLineRunner commandLineRunner(RabbitMQMessageProducer rabbitMQMessageProducer,
+                                        NotificationConfig notificationConfig){
+        return args ->{
+            rabbitMQMessageProducer.publish(notificationConfig.getInternalExchange(),
+                    notificationConfig.getInternalNotificationRoutingKey(),
+                    "foo");
+        };
+    }*/
 }
